9e0880f344f68aff98f3da633b35454d8da5f39c
Jack Wong <jack7a5a1993@icloud.com>
2018-08-27 11:51:21 +0900

    able to show scanned card in imageview

--- LoadScannerDataSample/LoadScannerDataSample/View/File/FileCell.swift
+++ LoadScannerDataSample/LoadScannerDataSample/View/File/FileCell.swift
@@ -10,6 +10,7 @@ import UIKit
 
 final class FileCell: UITableViewCell {
     
+    @IBOutlet weak var cardImageView: UIImageView!
     @IBOutlet weak var fileNameLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -20,7 +21,15 @@ final class FileCell: UITableViewCell {
     
     var fileName: String? {
         didSet{
-            fileNameLabel.text = fileName
+            guard let cardFilePath = fileName,
+                  let image = UIImage(contentsOfFile: cardFilePath),
+                  let cgImage = image.cgImage  else {
+                return
+            }
+           let orientation = image.imageOrientation
+            print(orientation.rawValue)
+            cardImageView.image = UIImage(cgImage: cgImage, scale: image.scale, orientation: .right)
+            
         }
     }
 }
